dcec8f25e6f838d4469bd53940c23915d3182461
Edson Andrade <44776352+edsonacf@users.noreply.github.com>
2020-05-08 10:30:16 -0300

    Update trabalho2.c

--- Trabalho_2/trabalho2.c
+++ Trabalho_2/trabalho2.c
@@ -39,3 +39,14 @@ void cria_lista(){
     free(aux);
     return;
 }
+//Transfere o primeiro elemento da fila 1 para o final da fila 2
+//fila **f1 - ponteiro da fila que tera seu elemento excluido da lista
+//fila **f2 - ponteiro da fila que tera um elemento adicionado à lista
+void troca_elementos(fila **f1, fila **f2){
+    
+    //Elemento *fila auxiliar
+    fila *aux;
+
+    //Proucura o último elemento em **f2, caso a lista esteja vazia, cria o primeiro elemento
+    aux = *f2;
+    if(aux != NULL){
