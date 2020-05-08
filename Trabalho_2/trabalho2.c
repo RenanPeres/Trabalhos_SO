903a3099a5714fd04cd9c350012b0c2cdb84ff0a
RenanPeres <40309046+RenanPeres@users.noreply.github.com>
2020-05-08 02:22:38 -0300

    Criando conceitos globais e função
    Criado as constantes e variáveis globais para o processos produtores/consumidores;
    As structs para as listas de elementos;
    a função que inicia a lista de elementos do buffer.

--- Trabalho_2/trabalho2.c
+++ Trabalho_2/trabalho2.c
@@ -1,3 +1,37 @@
 //Login: gso14
 //Trabalho de Sistemas Operacionais - Produtor X Consumidor
-//
+//Descrição das funções utilizadas - cria as funções chamadas pelos processos
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <syscall.h>
+
+void cria_lista(){
+
+    fila *aux;
+    aux = (fila*)malloc(sizeof(fila));
+    if(aux == NULL){
+        printf("Não foi possivel alocar memoria para a lista de buffer\n");
+        return(-1);
+    }
+    aux->mapa = BUFFER;
+    aux->prox = NULL;
+
+    inicio.entrada = aux;
+    for(int i = 1; i < TAM_BUFFER; i++){
+        aux->prox = (fila*)malloc(sizeof(fila));
+        if(aux->prox == NULL){
+            printf("Não foi possivel alocar memoria para a lista de buffer\n");
+            return(-1);
+        }aux = aux->prox;
+        aux->mapa = &BUFFER[i];
+        aux->prox = NULL;
+    }
+    
+    inicio.saida = NULL;
+    aux = NULL;
+    free(aux);
+    return;
+}
--- Trabalho_2/trabalho2.h
+++ Trabalho_2/trabalho2.h
@@ -1,3 +1,31 @@
 //Login: gso14
 //Trabalho de Sistemas Operacionais - Produtor X Consumidor
-//
+//Declaração da biblioteca de funções utilizadas - declara as structs e as funções
+
+#ifndef BIBLIOTECA_T2
+#define BIBLIOTECA_T2
+
+
+#define TAM_BUFFER 20;
+#define TRUE 1;
+#define FALSE 0;
+
+int BUFFER[TAM_BUFFER];
+int espaco_livre = TAM_BUFFER;
+
+
+typedef struct{
+    int *mapa;
+    fila *prox;
+} fila;
+
+typedef struct{
+    fila *entrada;
+    fila *saida;
+} ref_fila;
+
+ref_fila inicio  = (ref_fila)malloc(sizeof(ref_fila));
+if(inicio == NULL){
+    printf("Não foi possivel alocar memoria para a lista de buffer\n");
+    exit(-1);
+}
