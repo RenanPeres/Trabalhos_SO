cface1ed33bdfd68390f70535e0425195f4894c2
RenanPeres <40309046+RenanPeres@users.noreply.github.com>
2020-05-08 02:04:06 -0300

    update main 1.0
    Add main criando a logica das threads que simularam os processos produtores e consumidores

---
+++ Trabalho_2/main.c
@@ -0,0 +1,34 @@
+//Login:gso14
+//Trabalho de Sistemas Operacionais - Produtor X Consumidor
+//Código main - Cria as threads dos processos produtores e consumidores e espera até que sejam concluidas
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+
+int main(){
+    pthread t1, t2; //Thread do processo produtor e consumidor
+    int p, c, r[2]; //Variável que carrega o status de sucesso ou erro de criação
+
+    cria_lista(); //Cria e inicializa as listas de buffer livre e cheio
+    printf("Foram lidos, no buffer:\n\n");
+    p = pthread_create(&t1, NULL, &prdoutor, NULL);//Inicia e testa o processo produtor
+    if(p){
+        fprintf(stderr,"ERRO - pthread_create() retornou: %d\n",p);
+        exit(EXIT_FAILURE);
+    }
+    c = pthread_create(&t2, NULL, &consumidor, NULL);//Inicia e testa o processo consumidor
+    if(c){
+        fprintf(stderr,"ERRO - pthread_create() retornou: %d\n",c);
+        exit(EXIT_FAILURE);
+    }
+
+    //Espera a conclusão das threads
+    r[0] = pthread_join(t1, NULL);
+    r[1] = pthread_join(t2, NULL);
+
+    //Verifica se as threads encerraram corretamente
+    if(r[0] == 0 && r[1] == 0) exit(EXIT_SUCCESS);
+    else exit(EXIT_FAILURE);
+}
