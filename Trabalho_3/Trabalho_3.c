fc685ab4c4f9c2f9315c7d846155c2656502c7ec
RenanPeres <renanperes99@usp.br>
2020-07-05 12:59:17 -0300

    Create Trabalho_3.c

---
+++ Trabalho_3/Trabalho_3.c
@@ -0,0 +1,7 @@
+//Login: gso14
+//Trabalho de Sistemas Operacionais - Gerenciamento de memória
+//Programa com simulador  de gerenciamento de memória virtual com paginação (algorítmo do tipo relógio)
+
+//Bliblioteca
+#include <stdio.h>
+#include <stdlib.h>
